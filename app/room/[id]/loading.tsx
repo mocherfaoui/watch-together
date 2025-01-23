4b9d05990027cbbae51e507db98834c9b1f65832
Mohammed Cherfaoui <hi@cherfaoui.dev>
2025-01-23 23:28:54 +0100

    small UI change for when stream is loading

--- app/room/[id]/loading.tsx
+++ app/room/[id]/loading.tsx
@@ -11,7 +11,7 @@ export default function Loading() {
           <Skeleton className='w-full h-full rounded-none' />
         </div>
       </div>
-      <div className='bg-white h-full w-full md:w-[40vw] lg:w-[25vw] md:border-l border-gray-200'>
+      <div className='bg-white h-full w-full md:w-[40vw] lg:w-[25vw] border-t md:border-l md:border-t-0 border-gray-200'>
         <div className='min-h-[61px] flex border-b border-gray-200 p-4 px-3 items-center'>
           <Skeleton className='h-[28px] w-[140px]' />
         </div>
--- components/chat-area/index.tsx
+++ components/chat-area/index.tsx
@@ -72,7 +72,7 @@ export default function ChatArea({
 
   return (
     <>
-      <div className='bg-white h-full w-full md:w-[40vw] lg:w-[25vw] md:border-l border-gray-200'>
+      <div className='bg-white h-full w-full md:w-[40vw] lg:w-[25vw] border-t md:border-l md:border-t-0 border-gray-200'>
         <div className='flex flex-col h-full'>
           <div className='h-[61px] flex border-b border-gray-200 px-3 items-center justify-between'>
             <h3 className='text-base font-semibold uppercase'>Room Chat</h3>
