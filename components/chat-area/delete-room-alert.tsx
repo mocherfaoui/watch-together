764175c86e0e9445c4aa2365b83120fe44e92c0a
Mohammed Cherfaoui <hi@cherfaoui.dev>
2025-04-10 20:23:33 +0100

    update color variables in globals.css and enhance alert dialog functionality with variant support

--- components/chat-area/delete-room-alert.tsx
+++ components/chat-area/delete-room-alert.tsx
@@ -57,6 +57,7 @@ export default function DeleteRoomAlert({
             Cancel
           </AlertDialogCancel>
           <AlertDialogAction
+            variant='destructive'
             disabled={isDeletingRoom}
             onClick={() => startTransition(() => deleteRoomAction())}
             data-umami-event="Confirm Delete Room button"
